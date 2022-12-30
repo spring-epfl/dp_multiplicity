ac68be7f0af8583d249802f1279577733662a485
Bogdan Kulynych <hello@bogdankulynych.me>
2022-12-30 01:41:53 +0100

    Add uciml loaders

---
+++ uciml/test_loader.py
@@ -0,0 +1,16 @@
+import pytest
+from loader import load_dataset, targets
+
+
+dataset_names = list(targets.keys())
+
+
+@pytest.mark.parametrize("dataset_name", dataset_names)
+@pytest.mark.parametrize("one_hot_encode", [False, True])
+def test_load_dataset(dataset_name, one_hot_encode):
+    X, y = load_dataset(dataset_name, one_hot_encoded=one_hot_encode)
+    assert len(X) > 0
+    assert len(X) == len(y)
+
+    if one_hot_encode:
+        assert len(list(X.select_dtypes(include=["object", "category"]))) == 0
